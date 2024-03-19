d55b8155eeb9ef106a02aac215d318c4d0fe1e13
Anish Govind <anishg24@gmail.com>
2024-03-19 13:48:24 -0700

    Added some plugins

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -64,6 +64,18 @@ local plugins = {
   },
 
   {
+    "krivahtoo/silicon.nvim",
+    cmd = "Silicon",
+    build = "./install.sh build",
+    config = function()
+      require('silicon').setup({
+        font = 'JetBrainsMono=16',
+        theme = "Monokai Extended"
+      })
+    end,
+  },
+
+  {
     "anuvyklack/pretty-fold.nvim",
     event = "VimEnter",
     dependencies = {
@@ -194,6 +206,14 @@ local plugins = {
     opts = {},
     enabled = false
   },
+  {
+    "mg979/vim-visual-multi",
+    event = "BufWinEnter"
+  },
+
+  -- {
+  --   'JoosepAlviste/nvim-ts-context-commentstring',
+  -- },
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
@@ -203,10 +223,6 @@ local plugins = {
   -- All NvChad plugins are lazy-loaded by default
   -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
   -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
-}
+  }
 
 return plugins
